fe027d086ea63953d0818ba61b50e0728441bb54
Kazuma Sasajima <sorryforthedelayinsending@vg.pe.hu>
2024-03-15 23:10:39 +0900

    tsom

--- youtube/index.js
+++ youtube/index.js
@@ -42,17 +42,16 @@ function playJSON(obj) {
         }
 
         const contentAll = obj.youtube;
-        const main = document.querySelector("main")
         for (const content of contentAll) {
             if (minutes == content.openMin && seconds == content.openSec) {
-                const iframe = document.createElement('iframe')
+                let iframe = document.createElement('iframe')
                 iframe.src = `http://www.youtube.com/embed/${content.id}?${content.startEnd}&autoplay=1`;
                 iframe.style.top = content.top;
                 iframe.style.left = content.left;
                 iframe.style.width = content.width;
                 iframe.style.height = content.height;
                 iframe.style.zIndex = content.zIndex;
-                main.appendChild(iframe)
+                document.querySelector("main").appendChild(iframe)
 
                 window.setTimeout(function () {
                     iframe.remove()
@@ -77,7 +76,7 @@ function playJSON(obj) {
             seconds = 0;
             showTime.textContent = '00:00';
 
-            const allFrame = userList.querySelectorAll("iframe")
+            const allFrame = document.querySelectorAll("iframe")
             allFrame.forEach((eachFlame) => {
                 eachFlame.remove()
             })
